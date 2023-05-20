9a2c98dc11f868f42ab818321543711ce43e32c2
MaksymKryvohus <108764846+MaksymKryvohus@users.noreply.github.com>
2023-05-20 21:48:55 +0300

    1 komit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,13 +7,6 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 
-
-
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
-console.log(galleryItems);
-
 const galleryContainer = document.querySelector('.gallery');
 
 const galleryItemsMarkup = galleryItems.map(({ preview, original, description }) => {
