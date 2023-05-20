5c559b51132b94110e416fd9dfe6ae39cd800ed0
MaksymKryvohus <108764846+MaksymKryvohus@users.noreply.github.com>
2023-05-20 22:11:34 +0300

    Revert "1 komit"
    This reverts commit 9a2c98dc11f868f42ab818321543711ce43e32c2.

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,6 +7,13 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 
+
+
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
 const galleryContainer = document.querySelector('.gallery');
 
 const galleryItemsMarkup = galleryItems.map(({ preview, original, description }) => {
