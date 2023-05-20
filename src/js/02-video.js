8a3d3b1aab7dd6e023a8cbb1893b22e75be121d8
MaksymKryvohus <108764846+MaksymKryvohus@users.noreply.github.com>
2023-05-20 22:44:04 +0300

    что то пошло не так

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(function (event) {
+    const currentTime = event.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000));
+window.addEventListener('DOMContentLoaded', function () {
+    const savedTime = localStorage.getItem('videoplayer-current-time');
+    if (savedTime) {
+        player.setCurrentTime(savedTime);
+    }
+});
